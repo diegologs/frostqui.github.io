e0f592001eeab57c75eaac4c87f7419c4bad96b3
Frostqui <diego.lopezgarcia@hotmail.com>
2016-09-29 21:35:20 +0200

    Simplificado diseño de la pagina principal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
       $('.parallax').parallax();
        $('.kappa').kappa();
+
+       var GitHubCommits = require("github-commits");
+       var gitHubCommits = new GitHubCommits();
+ 
+       gitHubCommits.forUser("frostqui")
+            .currentWeekCommits()
+            .toArray(function(repositories){
+              console.log(repositories);
+            });
     });
