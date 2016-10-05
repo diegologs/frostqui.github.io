90376c1f07ddae6a84f9fe478b2b68a5b6eab7bc
Frostqui <diego.lopezgarcia@hotmail.com>
2016-10-05 19:44:27 +0200

    Reducido la altura de los posts, arreglo en el post de Jekyll

--- js/index.js
+++ js/index.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
       $('.parallax').parallax();
        $('.kappa').kappa();
-
-       var GitHubCommits = require("github-commits");
-       var gitHubCommits = new GitHubCommits();
- 
-       gitHubCommits.forUser("frostqui")
-            .currentWeekCommits()
-            .toArray(function(repositories){
-              console.log(repositories);
-            });
     });
