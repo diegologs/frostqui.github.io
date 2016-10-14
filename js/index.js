ee9b357ef54982e8ff6eeab3d8628c49da2d507b
Frostqui <diego.lopezgarcia@hotmail.com>
2016-10-14 21:33:47 +0200

    Cambio en el logo, añadido cambio en la pagina dependiendo de las estaciones

--- js/index.js
+++ js/index.js
@@ -1,4 +1,43 @@
 $(document).ready(function(){
       $('.parallax').parallax();
-       $('.kappa').kappa();
+      $('.kappa').kappa();
+   
+
+
+    var currentTime = new Date()
+    var month = currentTime.getMonth() + 1
+    var total = month;
+    var src = "/images/";
+  
+    
+    // Verano
+    if (total >=6 && total <= 7)
+    {
+        src += "logo_summer.jpg";
+
+    }
+    
+
+
+    // Navidad
+    else if (total ==12 || total == 1)
+   {
+     src += "logo_winter.jpg";
+     $('.menu').prepend('<img id="lights" src="/images/lights2.png" />')
+     $('.menu').prepend('<div id="candy"></div>')
+    }
+
+
+
+
+  else
+  {
+    src += "logo.png";
+        
+  }
+
+  $('.logo').attr("src", src);
+
     });
+
+
