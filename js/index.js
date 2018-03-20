247508f4811ba157a52644a3c45bfbb981f6b2a5
Frostqui <diego.lopezgarcia@hotmail.com>
2018-03-20 19:01:17 +0100

    Seguimos con el rediseño de toda la web 	- Eliminado código antiguo 	- Eliminado librerías antiguas 	- Añadido header con imagen que cambia en cada estación del año 	- Definida estructura para la lista de posts de la página principal

--- js/index.js
+++ js/index.js
@@ -1,45 +1,55 @@
-$(document).ready(function(){
-      $('.parallax').parallax();
-       $(".button-collapse").sideNav();
+(function () {
+  function getCurrentSeason() {
+    var now = new Date();
+    var currentYear = now.getFullYear();
 
-   
-
-
-    var currentTime = new Date()
-    var currentTime2 = new Date()
-    var month = currentTime.getMonth() + 1
-    var total = month;
-    var src = "/images/";
-
-    
-    // Verano
-    //if (total >=6 && total <= 7)
-   // {
-        
-
-   // }
-    
-
-
-    // Navidad
-     if (total ==12 || total == 0)
-   {
-     src += "logo_winter.png";
-     $('.container').prepend('<img id="lights" src="/images/lights2.png" />');
-     $('nav').append('<div id="candy"></div>');
+    if (now < new Date(currentYear, 2, 1)) {
+      // [Jan 1 ~ Mar 1)
+      return 'winter';
     }
-
-
-
-
-  else
-  {
-    src += "logo.png";
-        
+    if (now < new Date(currentYear, 5, 1)) {
+      // [Mar 1 ~ Jun 1)
+      return 'spring';
+    }
+    if (now < new Date(currentYear, 8, 1)) {
+      // [Jun 1 ~ Sep 1)
+      return 'summer';
+    }
+    if (now < new Date(currentYear, 11, 1)) {
+      // [Sep 1 ~ Dec 1)
+      return 'autumn';
+    }
+    // [Dec 1 ~ Dec 31]
+    return 'winter';
   }
 
-  $('.logo').attr("src", src);
+  let header = document.getElementById('header');
+  switch (getCurrentSeason()) {
+    case 'winter':
+      header.style.backgroundImage = "linear-gradient( rgba(86, 156, 230, 0.9), rgba(192, 63, 234, 0.9) ), url('/images/bgs/winter.gif')";
+    break;
+    case 'spring':
+      header.style.backgroundImage = "linear-gradient( rgba(67, 160, 145, 0.7), rgba(138, 142, 42, 0.9) ), url('/images/bgs/spring.gif')";
+    break;
+    case 'summer':
+      header.style.backgroundImage = "linear-gradient( rgba(229, 209, 119, 0.7), rgba(142, 42, 79, 0.9) ), url('/images/bgs/summer.gif')";
+    break;
+    case 'autumn':
+      header.style.backgroundImage = "linear-gradient( rgba(150, 103, 87, 0.7), rgba(132, 45, 126, 0.9) ), url('/images/bgs/autumm.gif')";
+    break;
+  }
+})();
 
-    });
 
+
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
